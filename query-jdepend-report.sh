f7c29e0001a04204932502f5771d4f6b05075108
Remi Emonet <remi.emonet@reverse--com.heeere>
2011-12-20 00:36:59 +0100

    tool

--- query-jdepend-report.sh
+++ query-jdepend-report.sh
@@ -1,32 +1,57 @@
 #!/bin/sh
 
-depsof() {
-  xpath  -e '//Package[UsedBy/Package/text() = "'$1'"]/@name' jdepend/report.xml  2> /dev/null | sed 's@.*name="\(.*\)"@\1@g' | sort
+verbose=1
+test "x$1" = x || verbose=0
+info() {
+  test $verbose = 0 || echo "INFO: $@" 1>&2
 }
 
-done=org.apache.commons.math.linear
-deps=$(depsof $done)
-count=$(echo "$deps" | wc -l)
-oldcount=0
-
-while test $oldcount -ne $count
-do
-    for i in $deps
+cumulate() {
+    done=$2
+    deps=$($1 $done)
+    count=$(echo "$deps" | wc -l)
+    oldcount=0
+    
+    while test $oldcount -ne $count
     do
-        if echo "$done" | grep -q $i
+        for i in $deps
+        do
+            if echo "$done" | grep -q '^'"$i"'$'
             then
-            echo "skip "$i
-        else
-            echo "process "$i
-            deps=$( (echo "$deps" ; depsof $i) | sort | uniq)
-            done=$( echo "$done" ; echo $i)
-        fi
+                info "skip " $i
+            else
+                info "process "$i
+                deps=$( (echo "$deps" ; $1 $i) | sort | uniq)
+                done=$( (echo "$done" ; echo $i) | sort | uniq)
+            fi
     done
     oldcount=$count
     count=$(echo "$deps" | wc -l)
-done
+    done
+    echo "$done"
+}
 
-echo "$deps"
+
+where=src/main/java/org/apache/commons/math/linear
+depsofclass() {
+    for i in $(find $where -name \*.java)
+    do
+        ii=${i##*/}
+        ii=${ii%.java}
+        cat $where/$1.java | grep -q $ii && echo $ii
+    done | sort
+}
+jdependreport=jdepend/report.xml
+depsof() {
+  xpath  -e '//Package[UsedBy/Package/text() = "'$1'"]/@name' $jdependreport  2> /dev/null | sed 's@.*name="\(.*\)"@\1@g' | sort
+}
+
+echo "PACKAGE REPORT (BASED ON JDEPEND):"
+cumulate depsof org.apache.commons.math.linear
+
+echo "FREE STYLE CLASS REPORT"
+cumulate depsofclass LUDecompositionImpl
+echo
 
 
 #xpath  -e '//Package[UsedBy/Package/text() = (//Package[UsedBy/Package/text() = "org.apache.commons.math.linear"]/@name)]/@name' jdepend/report.xml
